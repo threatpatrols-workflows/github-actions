0d8d94dc8c2dc8e547d589fb221a09aa74fa0e09
Nicholas de Jong <ndejong@threatpatrols.com>
2025-01-05 03:11:13 +1000

    Adjust the hello world message

--- hello-world/main.py
+++ hello-world/main.py
@@ -1,8 +1,7 @@
 import os
 
 def hello_world():
-    print("Hello World")
-    print(f"input: {os.getenv('INPUT_HELLO_WORLD')}")
+    print(f"Hello World: {os.getenv('INPUT_HELLO_WORLD')}")
 
 if __name__ == "__main__":
     hello_world()
