c1b9aa75fa79b98871d624876e0f7707d1f40005
Nicholas de Jong <ndejong@threatpatrols.com>
2025-01-05 02:59:59 +1000

    Use Dockerfile approach

--- hello-world/main.py
+++ hello-world/main.py
@@ -2,7 +2,7 @@ import os
 
 def hello_world():
     print("Hello World")
-    print(f"input: {os.getenv("INPUT_HELLO_WORLD")}")
+    print(f"input: {os.getenv('INPUT_HELLO_WORLD')}")
 
 if __name__ == "__main__":
     hello_world()
